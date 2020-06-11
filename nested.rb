4d66ff8bc07aa23008f38f11f0f8992ff5018735
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-11 00:05:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,8 +106,7 @@ def changing_alan
         :languages => ["C"]
       }
 }
-programmer_hash[:alan_kay][:known_for] = alan_new
-alan_new = "GUI"
+programmer_hash[:alan_kay][:known_for] = "GUI"
 programmer_hash
 end
 
